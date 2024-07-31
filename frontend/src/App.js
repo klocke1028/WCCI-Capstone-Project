b9a24eaa7549c0fdcd191d61e2d282012913c5f2
GRossMcDonald <ross_mcdonald@outlook.com>
2024-07-31 19:37:22 -0400

    finishing up backend now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
+import SearchForGames from './components/SearchForGames';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
 
   return (
-    <div></div>
+    <>
+      <div className="background-container"></div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SearchForGames />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
+
 
 export default App;
--- frontend/src/components/SearchForGames.js
+++ frontend/src/components/SearchForGames.js
@@ -9,33 +9,44 @@ function SearchForGames() {
 
   useEffect(() => {
     // Fetch the list of all games from Steam DB
-    const fetchAllGames = async () => {
-      const url = `https://api.steampowered.com/ISteamApps/GetAppList/v2/`;
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setAllGames(data.applist.apps);
-      } catch (error) {
-        console.error("There was a problem fetching the app list " + error);
-      }
+    const fetchAllGames = () => {
+      const url = `https://api.steampowered.com/ISteamApps/GetAppList/v2/`; /**Access to fetch at 'https://api.steampowered.com/ISteamApps/GetAppList/v2/' 
+      from origin 'http://localhost:3000' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. 
+      If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled. */
+      fetch(url, {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok.");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("Fetched games: ", data.applist.apps);
+          setAllGames(data.applist.apps);
+        })
+        .catch((error) => {
+          console.error("There was a problem fetching the app list: " + error);
+        });
     };
-
+    //Runs only on first render
     fetchAllGames();
   }, []);
 
   useEffect(() => {
     // Filter the results based on the search term
     /*The useEffect hook is used to run side-effects in a functional component. It takes two arguments: a function to execute and an array of dependencies that control when the effect runs.
-    allGames should now hold all the games that were fetched with the updater function setAllGames
+    allGames should now hold all the games that were fetched with the updater function setAllGames.
     game.name should grab the title of the games based on what I see from postman JSON body results.
     threw in lowercase to make sure the search is not case sensitive.*/
-    const filteredResults = allGames.filter(game =>
+    const filteredResults = allGames.filter((game) =>
       game.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
     //setResults updater function will update results to be a filtered array based on the search
+    //Runs on first render and any time a dependency value changes
     setResults(filteredResults);
   }, [searchTerm, allGames]);
 
