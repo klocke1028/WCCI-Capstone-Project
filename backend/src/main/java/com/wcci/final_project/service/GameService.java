24c782536892c2d0cd42810bffed93ae3d282935
IgnorantOracle <deidra.clay15@gmail.com>
2024-08-03 14:27:01 -0400

    backend search bar functionality complete

--- backend/src/main/java/com/wcci/final_project/service/GameService.java
+++ backend/src/main/java/com/wcci/final_project/service/GameService.java
@@ -47,46 +47,74 @@ public class GameService {
 		List<Game> searchResults = new ArrayList<>();
 		int resultsLimiter = 20;
 		String itadApiKey = "7f002b2417b6c356251e81434b37c25a3a28402d";
-		URL urlObj = new URL("https://api.isthereanydeal.com/games/search/v1?title=" + searchTerm + "&results=" + resultsLimiter + "&key=" + itadApiKey);
-        HttpsURLConnection itadConnection = (HttpsURLConnection) urlObj.openConnection();
+		URL searchGames = new URL("https://api.isthereanydeal.com/games/search/v1?title=" + searchTerm + "&results=" + resultsLimiter + "&key=" + itadApiKey);
+        HttpsURLConnection itadConnection = (HttpsURLConnection) searchGames.openConnection();
         itadConnection.setRequestMethod("GET");
 
         int responseCode = itadConnection.getResponseCode();
 
 		if (responseCode == HttpsURLConnection.HTTP_OK) {
-			BufferedReader  bufferedReader = new BufferedReader(new InputStreamReader(itadConnection.getInputStream()));
-			String inputLine = bufferedReader.readLine();
-            StringBuilder response = new StringBuilder();
+			BufferedReader  searchBufferedReader = new BufferedReader(new InputStreamReader(itadConnection.getInputStream()));
+			String searchInputLine = searchBufferedReader.readLine();
+            StringBuilder searchResponse = new StringBuilder();
 
-			response.append(inputLine);
+			searchResponse.append(searchInputLine);
 
-			bufferedReader.close();
-
-			ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode searchResultsNode = objectMapper.readTree(response.toString());
+			ObjectMapper searchObjectMapper = new ObjectMapper();
+            JsonNode searchResultsNode = searchObjectMapper.readTree(searchResponse.toString());
 
 			if (searchResultsNode.isArray()) {
                 for (JsonNode gameNode : searchResultsNode) {
 					String title = gameNode.path("title").asText();
 					String itadId = gameNode.path("id").asText();
-                    JsonNode assetsNode = gameNode.path("assets");
-                    Game game = new Game(title, itadId, boxArtLink);
-					searchResults.add(game);
+
+                    URL gameInfo = new URL("https://api.isthereanydeal.com/games/info/v2?id=" + itadId + "&key=" + itadApiKey);
+                    HttpsURLConnection gameInfoConnection = (HttpsURLConnection) gameInfo.openConnection();
+                    gameInfoConnection.setRequestMethod("GET");
+                    int gameInfoResponseCode = gameInfoConnection.getResponseCode();
+
+                    if (gameInfoResponseCode == HttpsURLConnection.HTTP_OK) {
+                        BufferedReader  gameBufferedReader = new BufferedReader(new InputStreamReader(gameInfoConnection.getInputStream()));
+                        String gameInputLine = gameBufferedReader.readLine();
+                        StringBuilder gameResponse = new StringBuilder();
+
+                        gameResponse.append(gameInputLine);
+
+                        ObjectMapper gameObjectMapper = new ObjectMapper();
+                        JsonNode gameInfoNode = gameObjectMapper.readTree(gameResponse.toString());
+                        String boxArtUrl = gameInfoNode.path("assets").path("boxart").asText();
+
+                        if (boxArtUrl.isEmpty()) {
+                            String banner600 = gameInfoNode.path("assets").path("banner600").asText();
+                            boxArtUrl = banner600;
+                        }
+
+                        if (boxArtUrl.isEmpty()) {
+                            String banner400 = gameInfoNode.path("assets").path("banner400").asText();
+                            boxArtUrl = banner400;
+                        }
+
+                        if (boxArtUrl.isEmpty()) {
+                            String banner300 = gameInfoNode.path("assets").path("banner300").asText();
+                            boxArtUrl = banner300;
+                        }
+
+                        if (boxArtUrl.isEmpty()) {
+                            String banner145 = gameInfoNode.path("assets").path("banner145").asText();
+                            boxArtUrl = banner145;
+                        }
+                        
+                        Game game = new Game(title, itadId, boxArtUrl);
+					    searchResults.add(game);
+                    } else {
+                        System.out.println("Error in getting game info. Error code: " + gameInfoResponseCode);
+                    }
                 }
             }
 		} else {
-			System.out.println("Error in getting game search results. Error code: " + responseCode);
+			System.out.println("Error in getting search results. Error code: " + responseCode);
 		}
 
 		return searchResults;
     }
-
-    // Modify this to do this: 
-    /*
-     * use Steam API
-     * First do a get all apps fetch to get IDs and titles 
-     * We want to loop through the games
-     * Store titles and IDs that contain the search term in their title into an array 
-     * Return the array
-     */
 }
--- backend/src/main/java/com/wcci/final_project/service/PriceAlertService.java
+++ backend/src/main/java/com/wcci/final_project/service/PriceAlertService.java
@@ -1,8 +1,15 @@
 package com.wcci.final_project.service;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wcci.final_project.entity.Game;
 import com.wcci.final_project.entity.PriceAlert;
 import com.wcci.final_project.repository.PriceAlertRepository;
 
@@ -11,6 +18,9 @@ public class PriceAlertService {
 
     @Autowired
     private PriceAlertRepository priceAlertRepository;
+
+    @Autowired
+    private GameService gameService;
 
     public PriceAlert createPriceAlert(PriceAlert priceAlert) {
         return priceAlertRepository.save(priceAlert);
@@ -27,5 +37,40 @@ public class PriceAlertService {
 
         priceAlertRepository.deleteById(id);
         return true;
+    }
+
+    public PriceAlert trackPrice(Long gameId) throws IOException {
+        PriceAlert priceAlert = new PriceAlert();
+
+        Game priceAlertGame = gameService.getGameById(gameId);
+        priceAlert.setGame(priceAlertGame);
+
+        String gameTitle = priceAlertGame.getTitle();
+
+        List<Integer> shopIds = getItadShopIds();
+
+        Runnable track = new Runnable() {
+            @Override
+            public void run() {
+                /*
+                 * URL getPriceUrl = new URL("");
+                 * HttpsURLConnection getPriceConnection = (HttpsURLConnection) getPriceUrl.openConnection(); 
+                 */
+                
+            }
+        };    
+    }
+
+    private List<Integer> getItadShopIds() throws IOException {
+        URL getShopsUrl = new URL("https://api.isthereanydeal.com/service/shops/v1?country=US");
+        HttpsURLConnection getShopsConnection = (HttpsURLConnection) getShopsUrl.openConnection();
+
+        getShopsConnection.setRequestMethod("GET");
+
+        int getShopsResponseCode = getShopsConnection.getResponseCode();
+
+        System.out.println(getShopsResponseCode);
+
+        
     }
 }
