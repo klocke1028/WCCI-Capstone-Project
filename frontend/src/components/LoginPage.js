00f1201f0094043c6fd534d3d9a9bcbb2d5f1048
klocke1028 <k.locke1028@gmail.com>
2024-08-03 09:59:38 -0400

    Login Page JS and CSS

---
+++ frontend/src/components/LoginPage.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./Login.css";
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="login">
+        <h4>Login</h4>
+        <form>
+          <div className="text_area">
+            <input
+              type="text"
+              id="username"
+              name="username"
+              defaultValue="E-mail"
+              className="text_input"
+            />
+          </div>
+          <input type="submit" value="LOGIN" className="button" />
+        </form>
+        <a className="link" href="/signup">
+          Sign Up
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Login;
