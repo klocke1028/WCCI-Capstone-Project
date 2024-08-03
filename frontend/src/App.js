9c32a05d3a7afd36311770b5e4bc46f7a9f8e693
GRossMcDonald <ross_mcdonald@outlook.com>
2024-08-02 21:37:15 -0400

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
-import SearchForGames from './components/SearchForGames';
+import React from 'react';
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 function App() {
-
   return (
-    <>
-      <div className="background-container"></div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<SearchForGames />} />
-        </Routes>
-      </Router>
-    </>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchForGames from "./SearchForGames";
+import "./Navbar.css";
+
+function NavBar() {
+  return (
+    <div className="navbar-wrapper">
+            <nav className="navbar">
+      <SearchForGames />
+    </nav>
+    </div>
+  );
+}
+
+export default NavBar;
