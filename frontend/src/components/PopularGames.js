018842f0a35500e1eb91fba75fd1653a276a0d3e
GRossMcDonald <ross_mcdonald@outlook.com>
2024-08-09 19:47:26 -0400

    This is pretty cool

--- frontend/src/components/PopularGames.js
+++ frontend/src/components/PopularGames.js
@@ -1,46 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const PopularGames = () => {
-    const [games, setGames] = useState([]);
+  const [games, setGames] = useState([]);
 
-    useEffect(() => {
-        const fetchPopularGames = () => {
-            const url = 'http://localhost:8080/api/games/popular';
-            fetch(url, {
-                headers: {
-                    Accept: "application/json",
-                },
-            })
-            .then ((response) => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok.");
-                }
-                return response.json();
-            })
-            .then((data) => {
-                setGames(data);
-            })
-            .catch((error) => {
-                console.log("There was a problem fetching popular games: " + error);
-            });
-        };
+  useEffect(() => {
+    const fetchPopularGames = () => {
+      const url = "http://localhost:8080/api/games/popular";
+      fetch(url, {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok.");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setGames(data);
+        })
+        .catch((error) => {
+          console.log("There was a problem fetching popular games: " + error);
+        });
+    };
 
-        fetchPopularGames();
-    }, []);
+    fetchPopularGames();
+  }, []);
 
-    return (
-        <div className="popular-games">
-            <h2>Popular Games</h2>
-            <ul>
-                {games.map((game) => (
-                    <li key={game.itadId}>
-                        <img src={game.boxArtLink} alt={game.title} />
-                        <h3>{game.title}</h3>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="popular-games">
+      <h2>Popular Games</h2>
+      <ul>
+        {games.map((game) => (
+          <li key={game.itadId}>
+            <Link to={`/GameInfoPage/${game.itadId}`}>
+              <img src={game.boxArtLink} alt={game.title} />
+              <h3>{game.title}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PopularGames;
