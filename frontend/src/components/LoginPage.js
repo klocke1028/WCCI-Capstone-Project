beb96652854bfb6a4940ab7cb02a119da4e5c8b1
klocke1028 <k.locke1028@gmail.com>
2024-08-08 21:37:52 -0400

    Account Creation

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./LoginPage.css";
+import LoginButton from "./LoginButton";
 
 function LoginPage() {
   return (
     <div>
-      <h2 className="login_page_header">Insert website name here :)</h2>
       <div className="login">
         <h4 className="login_form_header">Login</h4>
         <form className="login_form">
@@ -17,7 +17,7 @@ function LoginPage() {
               className="text_input"
             />
           </div>
-          <button type="submit">LOGIN</button>
+          <LoginButton />
         </form>
         <a className="link" href="/AccountRegistration">
           No account? Click here to sign Up!
